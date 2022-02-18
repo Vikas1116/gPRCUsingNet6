617053957b080e527c8e317bd602151d6f443b8f
Vikas Shankari <vshankari@ercbpo.com>
2022-02-18 16:50:34 +0530

    gprc demo

---
+++ GrpcNet6/gRPCClient/Program.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Net.Client;
+using GrpcNet6;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7192");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+       new GrpcNet6.HelloRequest { Name = "gRPC Demo" }
+);
+Console.WriteLine("from server: " +reply);
